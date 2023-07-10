70d564e6a233c313a7d812bd2c95e104b98901c3
christopherparker1923 <christmas1923@gmail.com>
2023-07-09 21:40:05 -0400

    image parsing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,39 @@
-import openai
+import cv2
+from PIL import Image
+import matplotlib.pyplot as pyplot
+import numpy as np
 
-openai.api_key = "secret key"
+pyplot.rcParams["figure.figsize"] = (10, 10)
 
-response = openai.Image.create(prompt="a mammoth", n=1, size="1024x1024")
+# Open the image file
+image = Image.open("./assets/bird.jpg")
+# image.show()
 
-print(response)
+image_array = np.array(image)
+
+# pyplot.imshow(image_array)
+pyplot.axis("off")
+
+
+#print("hello")
+
+
+# detected_edges = cv2.Canny(image_array, 100, 100)
+
+# inverted_image = 255 - detected_edges
+
+# pyplot.imshow(inverted_image, cmap="gray")
+
+
+# cv2.imwrite("./assets/bird_edges.png", cv2.cvtColor(inverted_image, cv2.COLOR_RGB2BGR))
+
+
+gray_image = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+
+# pyplot.imshow(gray_image, cmap="gray")
+
+cv2.imwrite("./assets/bird_grey.png", gray_image)
+
+pyplot.contour(gray_image, origin="image")
+
+pyplot.show()
